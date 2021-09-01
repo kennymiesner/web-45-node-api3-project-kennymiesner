2b4b7c95272a173afb5e605e8dd3627c3292cc5c
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-01 18:35:11 -0500

    Adds PUT request for updating user

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -29,10 +29,15 @@ router.post('/', validateUser, (req, res, next) => {
     .catch(next)
 });
 
-router.put('/:id', validateUserId, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  User.update(req.params.id, req.body)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(next)
 });
 
 router.delete('/:id', validateUserId, (req, res) => {

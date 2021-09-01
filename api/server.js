07987e1825459c8e65fad98d1f0ef4e74d4d6eb6
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-01 15:22:37 -0500

    Adds custom logger middleware

--- api/server.js
+++ api/server.js
@@ -4,7 +4,14 @@ const server = express();
 
 server.use(express.json())
 
-// global middlewares and the user's router need to be connected here
+function logger(req, res, next) {
+  console.log(`Request Method: ${req.method}`)
+  console.log(`Request URL: ${req.url}`)
+  console.log(`Timestamp: ${Date.now()}`)
+  next()
+}
+
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
